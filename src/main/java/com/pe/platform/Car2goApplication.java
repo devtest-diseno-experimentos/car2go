3b98d5f6fb9cf9ce74d35f02a9bd06e5d5736e7e
H4RRY73 <103443967+H4RRY73@users.noreply.github.com>
2024-10-18 19:54:21 -0500

    Initial commit

---
+++ src/main/java/com/pe/platform/Car2goApplication.java
@@ -0,0 +1,13 @@
+package com.pe.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Car2goApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Car2goApplication.class, args);
+	}
+
+}
