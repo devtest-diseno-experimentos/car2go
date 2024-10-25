101f2425a82039c48499be54d35a3d005f1aabc2
Jean Franco <u202219284@upc.edu.pe>
2024-10-25 12:15:39 -0500

    feat(vehicle): added vehicle controller

---
+++ src/main/java/com/pe/platform/vehicle/interfaces/rest/VehicleController.java
@@ -0,0 +1,104 @@
+package com.pe.platform.vehicle.interfaces.rest;
+
+import com.pe.platform.vehicle.domain.model.aggregates.Vehicle;
+import com.pe.platform.vehicle.domain.model.commands.UpdateVehicleCommand;
+import com.pe.platform.vehicle.domain.model.queries.*;
+import com.pe.platform.vehicle.domain.services.VehicleCommandService;
+import com.pe.platform.vehicle.domain.services.VehicleQueryService;
+import com.pe.platform.vehicle.interfaces.rest.resources.CreateVehicleResource;
+import com.pe.platform.vehicle.interfaces.rest.resources.VehicleResource;
+import com.pe.platform.vehicle.interfaces.rest.resources.UpdateVehicleResource;
+import com.pe.platform.vehicle.interfaces.rest.transform.CreateVehicleCommandFromResourceAssembler;
+import com.pe.platform.vehicle.interfaces.rest.transform.VehicleResourceFromEntityAssembler;
+import com.pe.platform.vehicle.interfaces.rest.transform.UpdateVehicleCommandFromResourceAssembler;
+import com.pe.platform.iam.infrastructure.authorization.sfs.model.UserDetailsImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/api/v1/vehicle")
+public class VehicleController {
+    private final VehicleCommandService vehicleCommandService;
+    private final VehicleQueryService vehicleQueryService;
+
+    public VehicleController(VehicleCommandService vehicleCommandService, VehicleQueryService vehicleQueryService) {
+        this.vehicleCommandService = vehicleCommandService;
+        this.vehicleQueryService = vehicleQueryService;
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_SELLER')")
+    @PostMapping
+    public ResponseEntity<VehicleResource> createVehicle(@RequestBody CreateVehicleResource resource) {
+        Optional<Vehicle> vehicle = vehicleCommandService.handle(CreateVehicleCommandFromResourceAssembler.toCommandFromResource(resource));
+        return vehicle.map(resp ->
+                        new ResponseEntity<>(VehicleResourceFromEntityAssembler
+                                .toResourceFromEntity(resp), CREATED))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VehicleResource> getVehicleById(@PathVariable int id) {
+        Optional<Vehicle> vehicle = vehicleQueryService.handle(new GetVehicleByIdQuery(id));
+        return vehicle.map(resp -> ResponseEntity.ok(VehicleResourceFromEntityAssembler.toResourceFromEntity(resp)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/location/{location}")
+    public ResponseEntity<List<VehicleResource>> getAllByLocation(@PathVariable String location) {
+        var getAllVehicleByLocation = new GetAllVehicleByLocationQuery(location);
+        var vehicles = vehicleQueryService.handle(getAllVehicleByLocation);
+        if (vehicles.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        var vehicleResources = vehicles.stream()
+                .map(VehicleResourceFromEntityAssembler::toResourceFromEntity)
+                .toList();
+        return ResponseEntity.ok(vehicleResources);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<VehicleResource>> getAllVehicles() {
+        var vehicles = vehicleQueryService.handle(new GetAllVehicleQuery());
+        if (vehicles.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        var vehicleResources = vehicles.stream().map(VehicleResourceFromEntityAssembler::toResourceFromEntity).toList();
+        return ResponseEntity.ok(vehicleResources);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_OWNER')")
+    @PutMapping
+    public ResponseEntity<VehicleResource> updateVehicle(@RequestBody UpdateVehicleResource resource) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        long userId = userDetails.getId();
+
+        UpdateVehicleCommand command = UpdateVehicleCommandFromResourceAssembler.toCommandFromResource(resource);
+        Optional<Vehicle> updatedVehicleOptional = vehicleCommandService.handle(command);
+
+        return updatedVehicleOptional
+                .filter(updatedVehicle -> updatedVehicle.getProfileId() == userId)
+                .map(updatedVehicle -> ResponseEntity.ok(VehicleResourceFromEntityAssembler.toResourceFromEntity(updatedVehicle)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+    @GetMapping("/all/vehicles/profile/{profileId}")
+    public ResponseEntity<List<VehicleResource>> getAllVehiclesByProfileId(@PathVariable long profileId) {
+        var vehicles = vehicleQueryService.handle(new GetAllVehicleByProfileId(profileId));
+        if (vehicles.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        var vehicleResources = vehicles.stream().map(VehicleResourceFromEntityAssembler::toResourceFromEntity).toList();
+        return ResponseEntity.ok(vehicleResources);
+    }
+
+}
