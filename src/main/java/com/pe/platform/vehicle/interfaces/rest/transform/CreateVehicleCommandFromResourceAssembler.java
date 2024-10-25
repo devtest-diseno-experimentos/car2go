82e883b375d55f0008db1b3552b32bd89199f527
Jean Franco <u202219284@upc.edu.pe>
2024-10-25 12:12:11 -0500

    chore(iam): added vehicle commands

---
+++ src/main/java/com/pe/platform/vehicle/interfaces/rest/transform/CreateVehicleCommandFromResourceAssembler.java
@@ -0,0 +1,30 @@
+package com.pe.platform.vehicle.interfaces.rest.transform;
+
+import com.pe.platform.vehicle.domain.model.commands.CreateVehicleCommand;
+import com.pe.platform.vehicle.interfaces.rest.resources.CreateVehicleResource;
+
+public class CreateVehicleCommandFromResourceAssembler {
+
+    public static CreateVehicleCommand toCommandFromResource(CreateVehicleResource resource) {
+        return new CreateVehicleCommand(
+                resource.name(),
+                resource.phone(),
+                resource.email(),
+                resource.brand(),
+                resource.model(),
+                resource.color(),
+                resource.year(),
+                resource.price(),
+                resource.transmission(),
+                resource.engine(),
+                resource.mileage(),
+                resource.doors(),
+                resource.plate(),
+                resource.location(),
+                resource.description(),
+                resource.image(),
+                resource.fuel(),
+                resource.speed()
+        );
+    }
+}
