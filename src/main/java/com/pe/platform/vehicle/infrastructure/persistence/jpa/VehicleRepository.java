490c6eea98f0ae019b47aad2abdbb7ce442a6570
Jean Franco <u202219284@upc.edu.pe>
2024-10-25 12:15:59 -0500

    feat(vehicle): added vehicle resource

---
+++ src/main/java/com/pe/platform/vehicle/infrastructure/persistence/jpa/VehicleRepository.java
@@ -0,0 +1,33 @@
+package com.pe.platform.vehicle.infrastructure.persistence.jpa;
+
+import com.pe.platform.vehicle.domain.model.aggregates.Vehicle;
+import jakarta.transaction.Transactional;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+
+    List<Vehicle> findAllByLocation(String location);
+
+    Optional<Vehicle> findById(@NotNull Integer id);
+
+    List<Vehicle> findByProfileId(Long profileId);
+
+    List<Vehicle> findByName(String name);
+
+    List<Vehicle> findAllVehiclesByProfileId(Long profileId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Vehicle v SET v.name = :name, v.location = :location, v.brand = :brand, v.model = :model, v.color = :color, v.year = :year, v.transmission = :transmission, v.engine = :engine, v.mileage = :mileage, v.doors = :doors, v.plate = :plate, v.description = :description, v.image = :image, v.price = :price, v.fuel = :fuel, v.speed = :speed WHERE v.id = :id")
+    Optional<Vehicle> updateById(Integer id);
+}
+
---
+++ src/main/java/com/pe/platform/vehicle/interfaces/rest/transform/VehicleResourceFromEntityAssembler.java
@@ -0,0 +1,32 @@
+package com.pe.platform.vehicle.interfaces.rest.transform;
+
+import com.pe.platform.vehicle.domain.model.aggregates.Vehicle;
+import com.pe.platform.vehicle.interfaces.rest.resources.VehicleResource;
+
+public class VehicleResourceFromEntityAssembler {
+
+    public static VehicleResource toResourceFromEntity(Vehicle entity) {
+        return new VehicleResource(
+                entity.getId(),
+                entity.getName(),
+                entity.getPhone(),
+                entity.getEmail(),
+                entity.getBrand(),
+                entity.getModel(),
+                entity.getColor(),
+                entity.getYear(),
+                entity.getPrice(),
+                entity.getTransmission(),
+                entity.getEngine(),
+                entity.getMileage(),
+                entity.getDoors(),
+                entity.getPlate(),
+                entity.getLocation(),
+                entity.getDescription(),
+                entity.getImage(),
+                entity.getProfileId(),
+                entity.getFuel(),
+                entity.getSpeed()
+        );
+    }
+}
