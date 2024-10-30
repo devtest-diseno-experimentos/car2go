14dd511d78d2b4f4061bc6e9b894b06bac936c12
Jean Franco <u202219284@upc.edu.pe>
2024-10-30 05:08:42 -0500

    feat(vehicle): updated vehicle repository

--- src/main/java/com/pe/platform/vehicle/infrastructure/persistence/jpa/VehicleRepository.java
+++ src/main/java/com/pe/platform/vehicle/infrastructure/persistence/jpa/VehicleRepository.java
@@ -26,6 +26,6 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
 
     @Modifying
     @Transactional
-    @Query("UPDATE Vehicle v SET v.name = :name, v.location = :location, v.brand = :brand, v.model = :model, v.color = :color, v.year = :year, v.transmission = :transmission, v.engine = :engine, v.mileage = :mileage, v.doors = :doors, v.plate = :plate, v.description = :description, v.image = :image, v.price = :price, v.fuel = :fuel, v.speed = :speed WHERE v.id = :id")
+    @Query("UPDATE Vehicle v SET v.name = :name, v.location = :location, v.brand = :brand, v.model = :model, v.color = :color, v.year = :year, v.transmission = :transmission, v.engine = :engine, v.mileage = :mileage, v.doors = :doors, v.plate = :plate, v.description = :description, v.price = :price, v.fuel = :fuel, v.speed = :speed WHERE v.id = :id")
     Optional<Vehicle> updateById(Integer id);
 }
